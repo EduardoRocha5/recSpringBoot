55a0a446ea37c0e121161fdfd6171099e7ec48b0
Eduardo Rocha <eduardorocha1049@gmail.com>
2025-10-28 20:28:22 -0300

    Feat: iniando recuperação

---
+++ src/main/java/com/recuperacao/recuperacao/RecuperacaoApplication.java
@@ -0,0 +1,17 @@
+package com.recuperacao.recuperacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecuperacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecuperacaoApplication.class, args);
+		System.out.println("Sistema Iniciado...");
+		System.out.println("http://localhost:8080");
+		
+
+	}
+
+}
