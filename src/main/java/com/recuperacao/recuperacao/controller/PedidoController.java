480d0d26d69b0537c767d0f4b2a8b5269a38a4b5
Eduardo Rocha <eduardorocha1049@gmail.com>
2025-11-02 19:08:33 -0300

    feat: estilizando com CSS

--- src/main/java/com/recuperacao/recuperacao/controller/PedidoController.java
+++ src/main/java/com/recuperacao/recuperacao/controller/PedidoController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -42,6 +43,15 @@ public class PedidoController {
         return"redirect:pedidos";
 
     }
+     @GetMapping("/excluir/{id}")
+        public String excluir(@PathVariable Long id) {
+
+        pedidoRepository.deleteById(id);
+
+        return "redirect:/pedidos";
+
+    }
+
 
 
 
